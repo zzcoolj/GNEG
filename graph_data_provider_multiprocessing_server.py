052bab5e47b86f89fe2b32a0a19205f9f592d50f
zzcoolj <zheng.zhang@limsi.fr>
2017-10-18 16:10:38 +0200

    run in server

--- graph_data_provider_multiprocessing_server.py
+++ graph_data_provider_multiprocessing_server.py
@@ -7,5 +7,5 @@ graph_data_provider_multiprocessing.multiprocessing_all(
     data_folder='/vol/corpusiles/open/Wikipedia-Dumps/en/20170420/prep/',
     file_extension='.txt',
     max_window_size=5,
-    process_num=10,
+    process_num=16,
     worker=graph_data_provider_multiprocessing.write_encoded_text_and_local_dict_for_txt)
