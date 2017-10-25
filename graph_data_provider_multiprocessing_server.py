c4eb7756a8988a84f4c2457b719556c550b8f64e
zzcoolj <zheng.zhang@limsi.fr>
2017-10-25 12:11:15 +0200

    server run test

--- graph_data_provider_multiprocessing_server.py
+++ graph_data_provider_multiprocessing_server.py
@@ -11,4 +11,4 @@ nltk.data.path = ['/vol/datailes/tools/nlp/nltk_data/2016']
 #     data_type='txt')
 # graph_data_provider_multiprocessing.merge_transferred_word_count()
 graph_data_provider_multiprocessing.write_valid_vocabulary()
-graph_data_provider_multiprocessing.multiprocessing_merge_edges_count_of_a_specific_window_size(window_size=50, process_num=30)
+graph_data_provider_multiprocessing.multiprocessing_merge_edges_count_of_a_specific_window_size(window_size=50, process_num=60)
