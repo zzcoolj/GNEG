2acd138c87cf047fea94e172e1d95046f4911f6d
zzcoolj <zheng.zhang@limsi.fr>
2017-11-03 16:18:51 +0100

    backup commit

--- graph_data_provider_multiprocessing_server.py
+++ graph_data_provider_multiprocessing_server.py
@@ -7,8 +7,5 @@ graph_data_provider_multiprocessing.prepare_intermediate_data(
     data_folder='/vol/corpusiles/open/Wikipedia-Dumps/en/20170420/prep/',
     file_extension='.txt',
     max_window_size=10,
-    process_num=50,
-    data_type='txt')
-# graph_data_provider_multiprocessing.merge_transferred_word_count()
-# graph_data_provider_multiprocessing.write_valid_vocabulary()
+    process_num=50)
 # graph_data_provider_multiprocessing.multiprocessing_merge_edges_count_of_a_specific_window_size(window_size=50, process_num=50)
