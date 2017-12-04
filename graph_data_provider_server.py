9885e6d9bbdecbed86357ba71391f7ad06e06895
zzcoolj <zheng.zhang@limsi.fr>
2017-12-04 16:56:33 +0100

    new server test command preprared

--- graph_data_provider_server.py
+++ graph_data_provider_server.py
@@ -3,13 +3,14 @@ import nltk.data
 
 nltk.data.path = ['/vol/datailes/tools/nlp/nltk_data/2016']
 
-graph_data_provider.prepare_intermediate_data(
-    data_folder='/vol/corpusiles/open/Wikipedia-Dumps/en/20170420/prep/',
-    file_extension='.txt',
-    max_window_size=10,
-    process_num=50)
-# graph_data_provider.multiprocessing_merge_edges_count_of_a_specific_window_size(window_size=5, process_num=50)
-# graph_data_provider.filter_edges(min_count=5,
-#                                  old_encoded_edges_count_path="output/intermediate data/graph/encoded_edges_count_window_size_5.txt")
-# graph_data_provider.reciprocal_for_edges_weight('output/intermediate data/graph/encoded_edges_count_window_size_5_undirected.txt')
-# graph_data_provider.merge_encoded_edges_count_for_undirected_graph('output/intermediate data/graph/encoded_edges_count_window_size_5_vocab_size_10000.txt')
+# graph_data_provider.prepare_intermediate_data(
+#     data_folder='/vol/corpusiles/open/Wikipedia-Dumps/en/20170420/prep/',
+#     file_extension='.txt',
+#     max_window_size=10,
+#     process_num=50)
+graph_data_provider.multiprocessing_merge_edges_count_of_a_specific_window_size(window_size=10, process_num=50)
+
+# # get undirected edges count for all file
+# for i in range(2, 11):
+#     file_path = 'output/intermediate data/encoded_edges_count_window_size_' + str(i) + '.txt'
+#     graph_data_provider.merge_encoded_edges_count_for_undirected_graph(file_path)
