86118c820c6204f40da4b8234192e29d0cd16d99
zzcoolj <zheng.zhang@limsi.fr>
2017-12-14 16:15:37 +0100

    gbw write grid search result into excel

--- graph_based_word2vec.py
+++ graph_based_word2vec.py
@@ -1,8 +1,10 @@
 import os
 from word2vec_gensim_modified import Word2Vec
+import pandas as pd
 import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
+
 import sys
 sys.path.insert(0, '../common/')
 import multi_processing
@@ -52,7 +54,7 @@ class GridSearch(object):
         The only reason the Word2Vec class needs index2word_path, merged_word_count_path, valid_vocabulary_path is to 
         get valid words' count.
         """
-        # TODO LATER a valid word count function in gdp
+        # TODO LATER a valid word count function in gdp, so as to transfer only one parameter for Word2Vec class.
 
         model = Word2Vec(sentences=sentences,
                          index2word_path=self.index2word_path,
@@ -64,15 +66,36 @@ class GridSearch(object):
                          negative=self.negative)
         word_vectors = model.wv
         # TODO NOW save wv
-        print(word_vectors.evaluate_word_pairs('data/evaluation data/wordsim353/combined.tab'))
         del model
 
+        ''' Result of evaluate_word_pairs contains 3 parts:
+        ((0.43915524919358867, 2.3681259690228147e-13),                                     Pearson
+        SpearmanrResult(correlation=0.44614214937080449, pvalue=8.8819867392097872e-14),    Spearman 
+        28.328611898016998)                                                                 ratio of pairs with unknown 
+                                                                                            words (float)
+        '''
+        evaluation = word_vectors.evaluate_word_pairs('data/evaluation data/wordsim353/combined.tab')
+        result = [multi_processing.get_file_name(ns_path),
+                  evaluation[0][0], evaluation[0][1], evaluation[1][0], evaluation[1][1], evaluation[2]]
+        print(result)
+        return result
+
     def grid_search(self, ns_folder=config['word2vec']['negative_samples_folder']):
-        self.one_search(ns_path=None)  # baseline: original word2vec: encoded_edges_count file are not used.
+        evaluation_result = self.one_search(ns_path=None)  # baseline: original word2vec
+        df = pd.DataFrame(columns=['NS file', 'Pearson correlation', 'Pearson pvalue', 'Spearman correlation',
+                                   'Spearman pvalue', 'Ration of pairs with OOV'])
+        df.loc[0] = evaluation_result
+
+        i = 1
         files = multi_processing.get_files_endswith(data_folder=ns_folder, file_extension='.pickle')
         for file in files:
-            # TODO NOW multiprocessing
-            self.one_search(ns_path=file)
+            evaluation_result= self.one_search(ns_path=file)
+            df.loc[i] = evaluation_result
+            i += 1
+
+        writer = pd.ExcelWriter('output.xlsx')
+        df.to_excel(writer, 'Sheet1')
+        writer.save()
 
 
 if __name__ == '__main__':
@@ -83,6 +106,6 @@ if __name__ == '__main__':
                     index2word_path=config['graph']['dicts_and_encoded_texts_folder'] + 'dict_merged.txt',
                     merged_word_count_path=config['graph']['dicts_and_encoded_texts_folder'] + 'word_count_all.txt',
                     valid_vocabulary_path=config['graph']['dicts_and_encoded_texts_folder'] + 'valid_vocabulary_min_count_5_vocab_size_10000.txt',
-                    workers=4, sg=sg, negative=20)
+                    workers=5, sg=sg, negative=20)
 
     gs.grid_search()
