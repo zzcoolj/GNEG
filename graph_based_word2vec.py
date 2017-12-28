393e9297323ef6dccdd60216db095e506db84db6
zzcoolj <zheng.zhang@limsi.fr>
2017-12-28 16:11:11 +0100

    add error handler

--- graph_based_word2vec.py
+++ graph_based_word2vec.py
@@ -188,11 +188,16 @@ class GridSearch_new(object):
         files = multi_processing.get_files_endswith(data_folder=ns_folder, file_extension='.npy')
         for file in files:
             nodes_path = re.search('(.*)_(.*)_step_rw_matrix.npy', file).group(1) + '_nodes.pickle'
-            for power in [0.25, 0.5, 0.75, 1]:
-                evaluation_result = self.one_search(matrix_path=file, row_column_indices_value_path=nodes_path,
-                                                    power=power)
-                df.loc[i] = evaluation_result
-                i += 1
+            for power in [0.1, 0.25, 0.5, 0.75, 1]:
+                try:
+                    evaluation_result = self.one_search(matrix_path=file, row_column_indices_value_path=nodes_path,
+                                                        power=power)
+                except:
+                    print('ERROR:', file, nodes_path)
+                    continue
+                else:
+                    df.loc[i] = evaluation_result
+                    i += 1
 
         writer = pd.ExcelWriter(ns_folder+'output.xlsx')
         df.to_excel(writer, 'Sheet1')
