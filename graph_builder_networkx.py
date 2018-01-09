3a6b46d6c88656d04c23eab5c0ab956d30bb0f4f
zzcoolj <zheng.zhang@limsi.fr>
2018-01-09 16:00:15 +0100

    server test

--- graph_builder_networkx.py
+++ graph_builder_networkx.py
@@ -47,12 +47,15 @@ class NoGraph:
         self.graph_index2wordId = graph_index2wordId
         self.cooccurrence_matrix = cooccurrence_matrix
 
-    def get_stochastic_matrix(self):
+    def get_stochastic_matrix(self, power=None):
         """
         A replacement of get_stochastic_matrix function NXGraph class.
         """
         vocab_size = self.cooccurrence_matrix.shape[0]
         stochastic_matrix = self.cooccurrence_matrix.copy()
+        # power co-occurrence if needed.
+        if power:
+            stochastic_matrix = np.power(stochastic_matrix, power)
         # remove self loop
         for i in range(vocab_size):
             stochastic_matrix[i][i] = 0
@@ -171,6 +174,7 @@ class NXGraph:
         return self.graph.nodes(), matrix
 
     def __get_stochastic_matrix(self):
+        # ATTENTION: for a big graph, this method consumes too much memory and calculation time.
         self.graph.remove_edges_from(list(nx.selfloop_edges(self.graph)))  # remove self loop
         if self.directed:
             directed_graph = self.graph
@@ -401,12 +405,12 @@ class GraphGridSearcher:
         self.ns_folder = ns_folder  # output folder
 
     def one_to_one(self, encoded_edges_count_file_path, directed, t):
-        # NXGraph version: too slow
-        graph = NXGraph.from_encoded_edges_count_file(encoded_edges_count_file_path, directed=directed)
-        graph.get_t_step_random_walk_stochastic_matrix(t=t, output_folder=self.ns_folder)
+        # # NXGraph version: too slow
+        # graph = NXGraph.from_encoded_edges_count_file(encoded_edges_count_file_path, directed=directed)
+        # graph.get_t_step_random_walk_stochastic_matrix(t=t, output_folder=self.ns_folder)
 
-        # no_graph = NoGraph(encoded_edges_count_file_path)
-        # no_graph.get_t_step_random_walk_stochastic_matrix(t=t, output_folder=self.ns_folder)
+        no_graph = NoGraph(encoded_edges_count_file_path)
+        no_graph.get_t_step_random_walk_stochastic_matrix(t=t, output_folder=self.ns_folder)
 
     def one_to_many(self, encoded_edges_count_file_path, directed, t_max):
         print(multi_processing.get_pid(), encoded_edges_count_file_path)
@@ -469,6 +473,7 @@ if __name__ == '__main__':
     grid_searcher.one_to_one(encoded_edges_count_file_path='output/intermediate data/graph/encoded_edges_count_window_size_10_undirected.txt',
                              directed=False,
                              t=1)
-    common.count_time(start_time)
+    # around 50 mins for window size 10
+    print(common.count_time(start_time))
     # grid_searcher.many_to_many(encoded_edges_count_file_folder=config['graph']['graph_folder'], directed=False, t_max=5,
     #                            process_num=3)
