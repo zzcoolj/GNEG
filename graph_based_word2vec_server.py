41f4643ed362549c2b2013ab82634aa8b6f8d1da
zzcoolj <zheng.zhang@limsi.fr>
2018-01-11 22:27:53 +0100

    server test

--- graph_based_word2vec_server.py
+++ graph_based_word2vec_server.py
@@ -8,34 +8,35 @@ import common
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# # Whole wiki data
-# print('timer starts')
-# start_time = time.time()
-#
-# sg = 1  # Only care about skip-gram
-# gs = gbw.GridSearch_new(training_data_folder='/dev/shm/zzheng-tmp/prep/',
-#                         index2word_path=config['graph']['dicts_and_encoded_texts_folder'] + 'dict_merged.txt',
-#                         merged_word_count_path=config['graph']['dicts_and_encoded_texts_folder'] + 'word_count_all.txt',
-#                         valid_vocabulary_path=config['graph']['dicts_and_encoded_texts_folder'] + 'valid_vocabulary_min_count_5_vocab_size_10000.txt',
-#                         workers=50, sg=sg, negative=20)
-#
-# # gs.grid_search()
-# gs.one_search(matrix_path=config['word2vec']['negative_samples_folder']+'encoded_edges_count_window_size_3_undirected_3_step_rw_matrix.npy',
-#               graph_index2wordId_path=config['word2vec']['negative_samples_folder'] + 'encoded_edges_count_window_size_3_undirected_nodes.pickle',
-#               power=0.5)
-#
-# print('time in seconds:', common.count_time(start_time))
-
-# small data
+# Whole wiki data
 print('timer starts')
 start_time = time.time()
 
 sg = 1  # Only care about skip-gram
-gs = gbw.GridSearch_new(training_data_folder='data/training data/Wikipedia-Dumps_en_20170420_prep',
-                        index2word_path='output/intermediate_data_for_small_corpus/dicts_and_encoded_texts/dict_merged.txt',
-                        merged_word_count_path='output/intermediate_data_for_small_corpus/dicts_and_encoded_texts/word_count_all.txt',
-                        valid_vocabulary_path='output/intermediate_data_for_small_corpus/dicts_and_encoded_texts/valid_vocabulary_min_count_5_vocab_size_10000.txt',
-                        workers=20, sg=sg, negative=20)
+gs = gbw.GridSearch_new(training_data_folder='/dev/shm/zzheng-tmp/prep/',
+                        index2word_path=config['graph']['dicts_and_encoded_texts_folder'] + 'dict_merged.txt',
+                        merged_word_count_path=config['graph']['dicts_and_encoded_texts_folder'] + 'word_count_all.txt',
+                        valid_vocabulary_path=config['graph']['dicts_and_encoded_texts_folder'] + 'valid_vocabulary_min_count_5_vocab_size_10000.txt',
+                        workers=50, sg=sg, negative=20)
 
-gs.grid_search(ns_folder='output/intermediate_data_for_small_corpus/negative_samples/')
+gs.one_search(matrix_path=None, graph_index2wordId_path=None, power=None)
+# gs.one_search(matrix_path=config['word2vec']['negative_samples_folder']+'encoded_edges_count_window_size_3_undirected_3_step_rw_matrix.npy',
+#               graph_index2wordId_path=config['word2vec']['negative_samples_folder'] + 'encoded_edges_count_window_size_3_undirected_nodes.pickle',
+#               power=0.5)
+
 print('time in seconds:', common.count_time(start_time))
+
+# # small data
+# # 20 cores 7862s for 99 tests
+# print('timer starts')
+# start_time = time.time()
+#
+# sg = 1  # Only care about skip-gram
+# gs = gbw.GridSearch_new(training_data_folder='data/training data/Wikipedia-Dumps_en_20170420_prep',
+#                         index2word_path='output/intermediate_data_for_small_corpus/dicts_and_encoded_texts/dict_merged.txt',
+#                         merged_word_count_path='output/intermediate_data_for_small_corpus/dicts_and_encoded_texts/word_count_all.txt',
+#                         valid_vocabulary_path='output/intermediate_data_for_small_corpus/dicts_and_encoded_texts/valid_vocabulary_min_count_5_vocab_size_10000.txt',
+#                         workers=20, sg=sg, negative=20)
+#
+# gs.grid_search(ns_folder='output/intermediate_data_for_small_corpus/negative_samples/')
+# print('time in seconds:', common.count_time(start_time))
--- negative_samples_generator_server.py
+++ negative_samples_generator_server.py
@@ -13,5 +13,5 @@ dicts_and_encoded_texts_folder = 'output/intermediate_data_for_small_corpus/dict
 grid_searcher = nsg.NegativeSamplesGenerator(ns_folder=ns_folder,
                                              valid_vocabulary_path=dicts_and_encoded_texts_folder +
                                                                    'valid_vocabulary_min_count_5_vocab_size_10000.txt')
-grid_searcher.many_to_many(encoded_edges_count_file_folder=encoded_edges_count_file_folder, directed=False, t_max=1,
+grid_searcher.many_to_many(encoded_edges_count_file_folder=encoded_edges_count_file_folder, directed=False, t_max=6,
                            process_num=9)
