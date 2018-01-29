cf2ea327f0f0717aaeb8a40499d88e41648e6de1
zzcoolj <zheng.zhang@limsi.fr>
2018-01-29 18:37:45 +0100

    rewrite ns for whole wiki data

--- all_in_one.py
+++ all_in_one.py
@@ -23,47 +23,49 @@ sg = 1  # Only care about skip-gram
 #
 # print('time in seconds:', common.count_time(start_time))
 
-# print('build ns')
-# start_time = time.time()
-# grid_searcher = nsg.NegativeSamplesGenerator(ns_folder='output/intermediate data/negative_samples_partial/',
-#                                              valid_vocabulary_path='output/intermediate data/dicts_and_encoded_texts/valid_vocabulary_partial_min_count_5_vocab_size_10000.txt')
-# grid_searcher.many_to_many(encoded_edges_count_file_folder='output/intermediate data/graph/', directed=False, t_max=5, process_num=window_size-1, partial=True)
-# print('time in seconds:', common.count_time(start_time))
-
-print('graph-based word2vec')
+print('build ns')
 start_time = time.time()
-
-# data/training data/Wikipedia-Dumps_en_20170420_prep
-gs = gbw.GridSearch_new(training_data_folder='/dev/shm/zzheng-tmp/prep/',
-                        index2word_path=config['graph']['dicts_and_encoded_texts_folder'] + 'dict_merged.txt',
-                        merged_word_count_path=config['graph']['dicts_and_encoded_texts_folder'] + 'word_count_partial.txt',
-                        valid_vocabulary_path=config['graph']['dicts_and_encoded_texts_folder'] + 'valid_vocabulary_partial_min_count_5_vocab_size_10000.txt',
-                        workers=60, sg=sg, size=200, units=units)
-# gs.grid_search(ns_folder='output/intermediate data/negative_samples_partial/')  # 116876.32733845711s
-# gs.one_search(matrix_path=None, graph_index2wordId_path=None, power=None, ns_mode_pyx=0)
-# gs.one_search(matrix_path='output/intermediate data/negative_samples_partial/encoded_edges_count_window_size_5_undirected_partial_3_step_rw_matrix.npy',
-#               graph_index2wordId_path='output/intermediate data/negative_samples_partial/encoded_edges_count_window_size_5_undirected_partial_nodes.pickle',
-#               power=0.75, ns_mode_pyx=1)
-
-# TODO NOW try negative=10
-df = pd.DataFrame(columns=[
-    # negative sampling source information
-    'negative', 'NS file', 'Graph window size', 'Directed/Undirected', 't-random-walk', 'power',
-    # wordsim353
-    'wordsim353_Pearson correlation', 'Pearson pvalue',
-    'Spearman correlation', 'Spearman pvalue', 'Ration of pairs with OOV',
-    # simlex999
-    'simlex999_Pearson correlation', 'Pearson pvalue',
-    'Spearman correlation', 'Spearman pvalue', 'Ration of pairs with OOV',
-    # questions-words
-    'sem_acc', '#sem', 'syn_acc', '#syn', 'total_acc', '#total'
-])
-negative = [5, 10, 15, 20]
-for i in range(4):
-    evaluation_result = gs.one_search(matrix_path=None, graph_index2wordId_path=None, power=None, ns_mode_pyx=0, negative=negative[i])
-    df.loc[i] = [str(negative[i])] + evaluation_result
-writer = pd.ExcelWriter('output/intermediate data/negative_samples_partial/' + 'negatives.xlsx')
-df.to_excel(writer, 'Sheet1')
-writer.save()
-
+# output/intermediate data/negative_samples_partial/
+# output/intermediate data/dicts_and_encoded_texts/valid_vocabulary_partial_min_count_5_vocab_size_10000.txt
+grid_searcher = nsg.NegativeSamplesGenerator(ns_folder='output/intermediate data/negative_samples/',
+                                             valid_vocabulary_path='output/intermediate data/dicts_and_encoded_texts/valid_vocabulary_min_count_5_vocab_size_10000.txt')
+grid_searcher.many_to_many(encoded_edges_count_file_folder='output/intermediate data/graph/', directed=False, t_max=5, process_num=window_size-1, partial=False)
 print('time in seconds:', common.count_time(start_time))
+
+# print('graph-based word2vec')
+# start_time = time.time()
+#
+# # data/training data/Wikipedia-Dumps_en_20170420_prep
+# gs = gbw.GridSearch_new(training_data_folder='/dev/shm/zzheng-tmp/prep/',
+#                         index2word_path=config['graph']['dicts_and_encoded_texts_folder'] + 'dict_merged.txt',
+#                         merged_word_count_path=config['graph']['dicts_and_encoded_texts_folder'] + 'word_count_partial.txt',
+#                         valid_vocabulary_path=config['graph']['dicts_and_encoded_texts_folder'] + 'valid_vocabulary_partial_min_count_5_vocab_size_10000.txt',
+#                         workers=60, sg=sg, size=200, units=units)
+# # gs.grid_search(ns_folder='output/intermediate data/negative_samples_partial/')  # 116876.32733845711s
+# # gs.one_search(matrix_path=None, graph_index2wordId_path=None, power=None, ns_mode_pyx=0)
+# # gs.one_search(matrix_path='output/intermediate data/negative_samples_partial/encoded_edges_count_window_size_5_undirected_partial_3_step_rw_matrix.npy',
+# #               graph_index2wordId_path='output/intermediate data/negative_samples_partial/encoded_edges_count_window_size_5_undirected_partial_nodes.pickle',
+# #               power=0.75, ns_mode_pyx=1)
+#
+# # TODO NOW try negative=10
+# df = pd.DataFrame(columns=[
+#     # negative sampling source information
+#     'negative', 'NS file', 'Graph window size', 'Directed/Undirected', 't-random-walk', 'power',
+#     # wordsim353
+#     'wordsim353_Pearson correlation', 'Pearson pvalue',
+#     'Spearman correlation', 'Spearman pvalue', 'Ration of pairs with OOV',
+#     # simlex999
+#     'simlex999_Pearson correlation', 'Pearson pvalue',
+#     'Spearman correlation', 'Spearman pvalue', 'Ration of pairs with OOV',
+#     # questions-words
+#     'sem_acc', '#sem', 'syn_acc', '#syn', 'total_acc', '#total'
+# ])
+# negative = [5, 10, 15, 20]
+# for i in range(4):
+#     evaluation_result = gs.one_search(matrix_path=None, graph_index2wordId_path=None, power=None, ns_mode_pyx=0, negative=negative[i])
+#     df.loc[i] = [str(negative[i])] + evaluation_result
+# writer = pd.ExcelWriter('output/intermediate data/negative_samples_partial/' + 'negatives.xlsx')
+# df.to_excel(writer, 'Sheet1')
+# writer.save()
+#
+# print('time in seconds:', common.count_time(start_time))
