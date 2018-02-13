401e27a8c3e7671fb7c575990d0b26687f6f2274
zzcoolj <zheng.zhang@limsi.fr>
2018-02-13 15:52:43 +0800

    server test for medium data

--- graph_data_provider.py
+++ graph_data_provider.py
@@ -555,7 +555,7 @@ def prepare_intermediate_data(data_folder, file_extension,
         max_vocab_size=max_vocab_size)
 
 
-def part_of_data(units, window_size, process_num,
+def part_of_data(units, window_size, process_num, output_folder,
                  dicts_folder=config['graph']['dicts_and_encoded_texts_folder'],
                  min_count=config['graph']['min_count'],
                  max_vocab_size=config['graph']['max_vocab_size']):
@@ -564,18 +564,20 @@ def part_of_data(units, window_size, process_num,
         exit()
     # edges files should be already existed
     word_count_name = 'word_count_partial.txt'
-    merge_transferred_word_count(word_count_folder=dicts_folder, output_folder=dicts_folder, file_name=word_count_name, units=units)
-    valid_vocabulary_name = dicts_folder + 'valid_vocabulary_partial_min_count_' + str(min_count) + '_vocab_size_' + str(max_vocab_size) + '.txt'
+    merge_transferred_word_count(word_count_folder=dicts_folder, output_folder=output_folder+'dicts_and_encoded_texts/', file_name=word_count_name, units=units)
+    valid_vocabulary_name = output_folder + 'dicts_and_encoded_texts/' + 'valid_vocabulary_partial_min_count_' + str(min_count) + '_vocab_size_' + str(max_vocab_size) + '.txt'
     write_valid_vocabulary(
-        merged_word_count_path=dicts_folder + word_count_name,
+        merged_word_count_path=output_folder + 'dicts_and_encoded_texts/' + word_count_name,
         output_path=valid_vocabulary_name,
         min_count=int(min_count),
         max_vocab_size=max_vocab_size)
     multiprocessing_merge_edges_count_of_a_specific_window_size(window_size=window_size, process_num=process_num,
-                                                                max_vocab_size=max_vocab_size, units=units)
+                                                                max_vocab_size=max_vocab_size, units=units,
+                                                                dicts_folder=output_folder + 'dicts_and_encoded_texts/',
+                                                                output_folder=output_folder + 'graph/')
     for i in range(2, window_size+1):
         # TODO LATER need multiprocessing
-        file_path = config['graph']['graph_folder'] + 'encoded_edges_count_window_size_' + str(i) + '_partial.txt'
+        file_path = output_folder + 'graph/' + 'encoded_edges_count_window_size_' + str(i) + '_partial.txt'
         merge_encoded_edges_count_for_undirected_graph(old_encoded_edges_count_path=file_path)
 
 
